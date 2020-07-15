423f378d636793c0b239bd4a109237589f5472c8
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-07-15 11:04:37 +0200

    chore(api): add scaffold

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port http://localhost:3000 !');
+});
