adb9fc04576c49863ac495a4c12dc454b76beff3
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-07-15 12:59:55 +0200

    feat(api): allow sending json

--- api/index.js
+++ api/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const importExcel = require('./shell');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
 app.post('/', function(req, res) {
 
-  console.log('EXCEL-TO-DMN-API: Starting import ...');
+  console.log('[EXCEL-TO-DMN-API]: starting import ...');
 
-  return importExcel().then(function(result) {
+  return importExcel(req.body).then(function(result) {
     const code = result.code;
     const output = result.output;
 
     if (code == 200) {
-      console.log('EXCEL-TO-DMN-API: Import done.');
+      console.log('[EXCEL-TO-DMN-API]: import done.');
     } else {
-      console.error('EXCEL-TO-DMN-API: Something went wrong: ' + output);
+      console.error('[EXCEL-TO-DMN-API]: something went wrong: ' + output);
     }
 
     res.sendStatus(code);
@@ -23,5 +29,5 @@ app.post('/', function(req, res) {
 });
 
 app.listen(3000, function() {
-  console.log('Example app listening on port http://localhost:3000 !');
+  console.log('[EXCEL-TO-DMN-API]: app listening on port http://localhost:3000 !');
 });
--- api/shell.js
+++ api/shell.js
@@ -1,20 +1,20 @@
 const shell = require('shelljs');
 
 const DEFAULT_BIN_PATH = 'bin/dmn-xlsx-cli-0.4.0-SNAPSHOT.jar';
-const DEFAULT_INPUTS = 'A,B';
-const DEFAULT_OUTPUTS = 'C';
+const DEFAULT_INPUT_COLUMNS = 'A,B';
+const DEFAULT_OUTPUT_COLUMNS = 'C';
 const DEFAULT_INPUT_FILE = '~/pet-projects/excel-to-dmn-plugin/example.xlsx';
 const DEFAULT_OUTPUT_FILE = 'file.dmn';
 
 const importExcel = function(options = {}) {
 
   const binPath = options.binPath || DEFAULT_BIN_PATH;
-  const inputs = options.inputs || DEFAULT_INPUTS;
-  const outputs = options.outputs || DEFAULT_OUTPUTS;
+  const inputColumns = options.inputColumns || DEFAULT_INPUT_COLUMNS;
+  const outputColumns = options.outputColumns || DEFAULT_OUTPUT_COLUMNS;
   const inputFile = options.inputFile || DEFAULT_INPUT_FILE;
   const outputFile = options.outputFile || DEFAULT_OUTPUT_FILE;
 
-  const command = `java -jar ${binPath} --inputs ${inputs} --outputs ${outputs} ${inputFile} ${outputFile}`;
+  const command = `java -jar ${binPath} --inputs ${inputColumns} --outputs ${outputColumns} ${inputFile} ${outputFile}`;
 
   // TODO(pinussilvestrus): test purposes only cleanup
   shell.rm([ outputFile ]);
